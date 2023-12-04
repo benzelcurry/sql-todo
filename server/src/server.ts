a621b6beb9c9cf4943b7ca13b44039b25f80d601
Benzel <benawhiting@gmail.com>
2023-12-04 14:37:01 -0800

    Replace server with more barebones setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,74 +1,12 @@
-/**
- * Setup express server.
- */
+import express, { Express, Request, Response } from 'express'
 
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
-import express, { Request, Response, NextFunction } from 'express';
-import logger from 'jet-logger';
+const app: Express = express()
+const port = 3000; // MAKE THIS A .ENV VARIABLE
 
-import 'express-async-errors';
-
-import BaseRouter from '@src/routes/api';
-import Paths from '@src/constants/Paths';
-
-import EnvVars from '@src/constants/EnvVars';
-import HttpStatusCodes from '@src/constants/HttpStatusCodes';
-
-import { NodeEnvs } from '@src/constants/misc';
-import { RouteError } from '@src/other/classes';
-
-
-// **** Variables **** //
-
-const app = express();
-
-
-// **** Setup **** //
-
-// Basic middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser(EnvVars.CookieProps.Secret));
-
-// Show routes called in console during development
-if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
-  app.use(morgan('dev'));
-}
-
-// Security
-if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
-  app.use(helmet());
-}
-
-// Add APIs, must be after middleware
-app.use(Paths.Base, BaseRouter);
-
-// Add error handler
-app.use((
-  err: Error,
-  _: Request,
-  res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction,
-) => {
-  if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
-    logger.err(err, true);
-  }
-  let status = HttpStatusCodes.BAD_REQUEST;
-  if (err instanceof RouteError) {
-    status = err.status;
-  }
-  return res.status(status).json({ error: err.message });
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, this is Express + TypeScript')
 });
 
-// Set static directory (js and css).
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-
-
-// **** Export default **** //
-
-export default app;
+app.listen(port, () => {
+  console.log(`[Server]: I am running at https://localhost:${port}`)
+})
