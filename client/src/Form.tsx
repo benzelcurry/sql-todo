f01f5bd4eee373691a73824bc8216733a94b0ce5
Benzel <benawhiting@gmail.com>
2023-12-04 12:08:55 -0800

    Update styling of todo form and App.css

--- client/src/Form.tsx
+++ client/src/Form.tsx
@@ -13,19 +13,17 @@ function Form() {
   })
 
   return (
-    <div>
-      <form>
-        <h3>Create a new todo</h3>
-        <label htmlFor="task">Task</label>
-        <input type="text" name="task" id="task" required />
-        <label htmlFor="importance">Importance</label>
-        <input type="number" name="importance" id="importance" required />
-        <label htmlFor="due-date">Due Date</label>
-        <input type="date" name="due-date" id="due-date" />
-        <label htmlFor="description">Description</label>
-        <input type="text" name="description" id="description" />
-      </form>
-    </div>
+    <form className='todo-form'>
+      <h3 className='form-heading'>Create a new todo</h3>
+      <label htmlFor="task">Task</label>
+      <input type="text" name="task" id="task" required />
+      <label htmlFor="importance">Importance</label>
+      <input type="number" name="importance" id="importance" required />
+      <label htmlFor="due-date">Due Date</label>
+      <input type="date" name="due-date" id="due-date" />
+      <label htmlFor="description">Description</label>
+      <textarea name="description" id="description" />
+    </form>
   )
 }
 
