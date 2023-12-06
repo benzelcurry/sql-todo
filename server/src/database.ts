25125c665aa2778aeb79b3012841f4c8d63de602
Benzel <benawhiting@gmail.com>
2023-12-06 15:30:11 -0800

    Configure app to use .env

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,8 +1,16 @@
 import mysql from 'mysql2'
+require('dotenv').config()
 
-mysql.createPool({
+const pool = mysql.createPool({
   host: '127.0.0.1',
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: 'kanban_app'
 }).promise()
+
+const result = pool.query('SELECT * FROM todo;')
+result.then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log(err)
+})
