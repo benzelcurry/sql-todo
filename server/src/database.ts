ab4c93c2b41609081dd34218f07c3ca0560fac75
Benzel <benawhiting@gmail.com>
2023-12-07 14:33:48 -0800

    Update db pool to use all env vars

--- server/src/database.ts
+++ server/src/database.ts
@@ -2,15 +2,16 @@ import mysql from 'mysql2'
 require('dotenv').config()
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
+  host: process.env.HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
-  database: 'kanban_app'
+  database: process.env.DB_NAME
 }).promise()
 
 const result = pool.query('SELECT * FROM todo;')
 result.then(res => {
-  console.log(res)
+  const rows = res[0]
+  console.log(rows)
 }).catch(err => {
   console.log(err)
 })
