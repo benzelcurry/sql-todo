929ede0c0a6b61ace320895aec7a27e204c5f3a4
Benzel <benawhiting@gmail.com>
2023-12-08 15:12:54 -0800

    Refactor database to use Postgres instead of MySQL

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,38 +1,48 @@
-import mysql, { ResultSetHeader } from 'mysql2'
+// import mysql, { ResultSetHeader } from 'mysql2'
+const pgp = require('pg-promise')()
 require('dotenv').config()
 
-import { ITodo } from './types'
+const db = pgp({ connectionString: `postgres://${process.env.PG_USN}:${process.env.PG_PW}@${process.env.PG_PORT}/${process.env.PG_NAME}`})
+// import { ITodo } from './types'
 
-const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB_NAME
-}).promise()
+db.many('SELECT * FROM weather;')
+  .then((data: any) => {
+    console.log(data)
+  })
+  .catch((error: any) => {
+    console.log(error)
+  })
 
-async function getTodo(id: number) {
-  const [rows] = await pool.query<ITodo[]>(`
-  SELECT *
-  FROM todo
-  WHERE id = ?
-  `, [id])
-  return rows[0]
-}
+// const pool = mysql.createPool({
+//   host: process.env.HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PW,
+//   database: process.env.DB_NAME
+// }).promise()
+
+// async function getTodo(id: number) {
+//   const [rows] = await pool.query<ITodo[]>(`
+//   SELECT *
+//   FROM todo
+//   WHERE id = ?
+//   `, [id])
+//   return rows[0]
+// }
 
 // (async () => {
 //   const todo = await getTodo(3)
 //   console.log(todo)
 // })()
 
-async function createTodo(title: string, importance: number) {
-  const [result] = await pool.query<ResultSetHeader>(`
-  INSERT INTO todo (title, importance)
-  VALUES (?, ?)
-  `, [title, importance])
+// async function createTodo(title: string, importance: number) {
+//   const [result] = await pool.query<ResultSetHeader>(`
+//   INSERT INTO todo (title, importance)
+//   VALUES (?, ?)
+//   `, [title, importance])
 
-  const id = result.insertId
-  return getTodo(id)
-}
+//   const id = result.insertId
+//   return getTodo(id)
+// }
 
 // (async () => {
 //   const todo = await createTodo('Task 5', 3)
