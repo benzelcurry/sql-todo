65f994fe2365fc8723ac9641bce0ee5275db237d
Benzel <benawhiting@gmail.com>
2023-12-12 15:29:50 -0800

    Add route for inserting todo into table

--- server/src/routes/todo.routes.ts
+++ server/src/routes/todo.routes.ts
@@ -2,10 +2,14 @@ import express, { Request, Response, NextFunction } from 'express'
 const router = express.Router()
 
 import {
-  todo_list
+  todo_list,
+  new_todo
 } from '../controllers/todo.controller'
 
 // Returns list of todos
 router.get('/', todo_list)
+
+// Inserts a new todo into table
+router.post('/', new_todo)
 
 export default router
