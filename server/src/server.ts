559c236e506f4206b80d72fa234e09f200643631
Benzel <benawhiting@gmail.com>
2023-12-12 14:27:03 -0800

    Set up cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,18 @@
 import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
 
 const app: Express = express()
 const port = 3000; // MAKE THIS A .ENV VARIABLE
+
+// Fill this out once allowed origins are known
+const allowedOrigins = ['http://localhost:3000']
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+}
+
+app.use(cors(options))
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is Express + TypeScript')
