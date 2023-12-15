4f01f5460fca9eb02709b19d09e682774d5ea851
Benzel <benawhiting@gmail.com>
2023-12-15 14:17:17 -0800

    Add route for deleting todos

--- server/src/routes/todo.routes.ts
+++ server/src/routes/todo.routes.ts
@@ -3,7 +3,8 @@ const router = express.Router()
 
 import {
   todo_list,
-  new_todo
+  new_todo,
+  delete_todo
 } from '../controllers/todo.controller'
 
 // Returns list of todos
@@ -11,5 +12,8 @@ router.get('/', todo_list)
 
 // Inserts a new todo into table
 router.post('/', new_todo)
+
+// Deletes a todo of specified ID from table
+router.post('/', delete_todo)
 
 export default router
