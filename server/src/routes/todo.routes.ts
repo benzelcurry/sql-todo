8dadb2a69cb448ec2a29e19dc3cac8d309672b10
Benzel <benawhiting@gmail.com>
2023-12-19 14:53:00 -0800

    Add a route for updating todos

--- server/src/routes/todo.routes.ts
+++ server/src/routes/todo.routes.ts
@@ -4,6 +4,7 @@ const router = express.Router()
 import {
   todo_list,
   new_todo,
+  update_todo,
   delete_todo
 } from '../controllers/todo.controller'
 
@@ -12,6 +13,9 @@ router.get('/', todo_list)
 
 // Inserts a new todo into table
 router.post('/', new_todo)
+
+// Updates a todo of specified ID in the table
+router.patch('/', update_todo)
 
 // Deletes a todo of specified ID from table
 router.delete('/', delete_todo)
