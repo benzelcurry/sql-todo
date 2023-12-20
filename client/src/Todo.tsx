923cc2d9c5845784b951aa767e630ebf00bd96f5
Benzel <benawhiting@gmail.com>
2023-12-20 14:56:38 -0800

    Provide default values to form update input fields

--- client/src/Todo.tsx
+++ client/src/Todo.tsx
@@ -1,6 +1,6 @@
 // Creates Todo cards to be displayed in App
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios' 
 
 import './Todo.css'
@@ -10,6 +10,14 @@ import { ITodo } from './types'
 // TODO: Write logic to handle updates, style components, then separate into rows + add auth
 function Todo(props: ITodo) {
   const [activeUpdate, setActiveUpdate] = useState(false)
+  const [updatedTodo, setUpdatedTodo] = useState<ITodo>({
+    id: props.id,
+    title: props.title,
+    importance: props.importance,
+    due_date: props.due_date,
+    created: props.created,
+    description: props.description
+  })
 
   const handleDelete = (id: number) => {
     axios.delete('http://localhost:3000/todo', { params: { id: id } })
@@ -22,9 +30,10 @@ function Todo(props: ITodo) {
     })
   }
 
-  const handleUpdate = (id: number) => {
+  const handleUpdate = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
     // Update logic will go here once a corresponding method is built on the server
     // Will need to add a state variable and update text lines to be fields that then submit to the server
+    e.preventDefault()
   }
 
   return (
@@ -45,22 +54,24 @@ function Todo(props: ITodo) {
         <>
           <form className='update-form'>
             <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" required
-              onChange={(e) => setTodo({ ...todo, title: e.target.value})}  
+            <input type="text" name="title" id="title" value={updatedTodo.title} required
+              onChange={(e) => setUpdatedTodo({ ...updatedTodo, title: e.target.value})}  
             />
             <label htmlFor="importance">Importance</label>
-            <input type="number" name="importance" id="importance" min="0" max="10" required 
-              onChange={(e) => setTodo({ ...todo, importance: parseInt(e.target.value) })} 
+            <input type="number" name="importance" value={updatedTodo.importance} id="importance" min="0" max="10" required 
+              onChange={(e) => setUpdatedTodo({ ...updatedTodo, importance: parseInt(e.target.value) })} 
             />
             <label htmlFor="due_date">Due Date</label>
-            <input type="date" name="due_date" id="due_date" 
-              onChange={(e) => setTodo({ ...todo, due_date: new Date(e.target.value)})} 
+            <input type="date" name="due_date" id="due_date"
+              value={typeof updatedTodo.due_date === 'string' ? updatedTodo.due_date.split('T')[0] : undefined}  
+              onChange={(e) => setUpdatedTodo({ ...updatedTodo, due_date: new Date(e.target.value)})} 
             />
             <label htmlFor="description">Description</label>
-            <textarea name="description" id="description" 
-              onChange={(e) => setTodo({ ...todo, description: e.target.value})} 
+            <textarea name="description" id="description"
+              value={updatedTodo.description !== 'undefined' ? updatedTodo.description : '' }  
+              onChange={(e) => setUpdatedTodo({ ...updatedTodo, description: e.target.value})} 
             />
-            <button className='submit-btn'>Submit Changes</button>
+            <button className='submit-btn' onClick={(e) => handleUpdate(e, updatedTodo.id!)}>Submit Changes</button>
             <button className='cancel-btn' onClick={() => setActiveUpdate(!activeUpdate)}>Cancel Changes</button>
           </form>
         </>
--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -2,7 +2,7 @@ export type ITodo = {
   id?: number;
   title: string;
   importance: number;
-  due_date: Date;
+  due_date: Date | string;
   created: Date;
   description: string;
 }
